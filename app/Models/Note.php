5f074b4478f4dfbc9aeb57fa7ddc09cb9746bb68
Gene Kelley <phpcodehead@gmail.com>
2024-06-25 17:11:53 -0700

    Add User <> Note model relationship.

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Note extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'title', 'body'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
